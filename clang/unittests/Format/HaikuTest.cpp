00f167697c693a35a57aef377b96e0336b5d4821
Owen Pan <owenpiano@gmail.com>
2023-10-08 03:06:01 -0700

    Always run messUp() on Haiku test cases

--- clang/unittests/Format/HaikuTest.cpp
+++ clang/unittests/Format/HaikuTest.cpp
@@ -8,60 +8,75 @@
 
 #include "FormatTestBase.h"
 
-#define DEBUG_TYPE "haiku-test"
+#define verifyHaiku(...) _verifyHaikuFormat(__FILE__, __LINE__, __VA_ARGS__)
 
 namespace clang {
 namespace format {
 namespace test {
 namespace {
 
-class HaikuTest : public FormatTestBase {};
+class HaikuTest : public FormatTestBase {
+  const FormatStyle Style{getHaikuStyle()};
+
+protected:
+  void _verifyHaikuFormat(const char *File, int Line, StringRef Expected,
+                          StringRef Code,
+                          const std::vector<tooling::Range> &Ranges = {}) {
+    testing::ScopedTrace t(File, Line, testing::Message() << Code.str());
+    EXPECT_EQ(Expected.str(),
+              format(Expected, Style, SC_ExpectComplete, Ranges))
+        << "Expected code is not stable";
+    EXPECT_EQ(Expected.str(), format(Code, Style, SC_ExpectComplete, Ranges));
+    EXPECT_EQ(Expected.str(),
+              format(messUp(Code), Style, SC_ExpectComplete, Ranges));
+  }
+
+  void _verifyHaikuFormat(const char *File, int Line, StringRef Code) {
+    _verifyHaikuFormat(File, Line, Code, Code);
+  }
+};
 
 TEST_F(HaikuTest, BreakConstructorInitializers) {
-  FormatStyle Style = getHaikuStyle();
+  verifyHaiku("Foo::Foo(int32 param)\n"
+              "\t:\n"
+              "\tfMember(param),\n"
+              "\tfPointerMember(NULL)\n"
+              "{\n"
+              "}\n");
 
-  verifyFormat("Foo::Foo(int32 param)\n"
-               "\t:\n"
-               "\tfMember(param),\n"
-               "\tfPointerMember(NULL)\n"
-               "{\n"
-               "}\n",
-               Style);
-
-  verifyFormat("class BreakCtorInitializers\n"
-               "{\n"
-               "private:\n"
-               "\tint a, b;\n"
-               "\n"
-               "public:\n"
-               "\tBreakCtorInitializers(int i)\n"
-               "\t\t:\n"
-               "\t\ta(i)\n"
-               "\t{\n"
-               "\t\tb = 0;\n"
-               "\t}\n"
-               "\tBreakCtorInitializers(int i, int j);\n"
-               "};\n"
-               "\n"
-               "\n"
-               "BreakCtorInitializers::BreakCtorInitializers(int i, int j)\n"
-               "\t:\n"
-               "\ta(i),\n"
-               "\tb(j)\n"
-               "{\n"
-               "}\n",
-               "class BreakCtorInitializers {\n"
-               "  private: int a, b;\n"
-               "\n"
-               "  public:\n"
-               "    BreakCtorInitializers(int i)\n"
-               "      : a(i) { b = 0; }\n"
-               "    BreakCtorInitializers(int i, int j);\n"
-               "};\n"
-               "\n"
-               "BreakCtorInitializers::BreakCtorInitializers(int i, int j)\n"
-               ": a(i), b(j) {}\n",
-               Style);
+  verifyHaiku("class BreakCtorInitializers\n"
+              "{\n"
+              "private:\n"
+              "\tint a, b;\n"
+              "\n"
+              "public:\n"
+              "\tBreakCtorInitializers(int i)\n"
+              "\t\t:\n"
+              "\t\ta(i)\n"
+              "\t{\n"
+              "\t\tb = 0;\n"
+              "\t}\n"
+              "\tBreakCtorInitializers(int i, int j);\n"
+              "};\n"
+              "\n"
+              "\n"
+              "BreakCtorInitializers::BreakCtorInitializers(int i, int j)\n"
+              "\t:\n"
+              "\ta(i),\n"
+              "\tb(j)\n"
+              "{\n"
+              "}\n",
+              "class BreakCtorInitializers {\n"
+              "  private: int a, b;\n"
+              "\n"
+              "  public:\n"
+              "    BreakCtorInitializers(int i)\n"
+              "      : a(i) { b = 0; }\n"
+              "    BreakCtorInitializers(int i, int j);\n"
+              "};\n"
+              "\n"
+              "BreakCtorInitializers::BreakCtorInitializers(int i, int j)\n"
+              ": a(i), b(j) {}\n");
 }
 
 } // namespace
